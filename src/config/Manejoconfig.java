3243f43169dd07f70cf80137511caf2943ec4787
Danny_PC <Danny_PC@Equipo-0c10>
2020-03-29 20:49:39 -0600

    modificacion extrema xD

---
+++ src/config/Manejoconfig.java
@@ -0,0 +1,156 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package config;
+import java.io.File;
+import java.io.IOException;
+import org.ini4j.*;
+import ventanas.FrmConfiguracion;
+/**
+ *
+ * @author Danny_PC
+ */
+public class Manejoconfig {
+ /**
+ *lee el archivo config.ini y
+ * retorna un vector ==> motor-basededatos-servidor-usuario-contraseña
+     * @return 
+ */
+    
+    public String[] leer() {
+        String vect[]=new String[5];
+    
+        try{
+             //obtenemos la ubicacion del archivo y quitamos los caracteres del inicio inecesarios.
+             String ruta= (String.valueOf( ClassLoader.getSystemResource("config.ini")).substring(6)) ;
+             System.out.println("ruta al leer:"+ruta);
+            
+            
+            Wini ini = new Wini(new File(ruta));
+            String servidor = ini.get("database", "servidor", String.class);
+            String usuario = ini.get("database", "usuario", String.class);
+            String contraseña = ini.get("database", "contraseña", String.class);
+            String motor = ini.get("database", "motor", String.class);
+            String basededatos = ini.get("database", "basededatos", String.class);
+        
+            
+            System.out.println("motor: "+ motor);
+            System.out.println("basededatos: "+ basededatos);
+            System.out.println("servidor: "+ servidor);
+            System.out.println("usuario: "+ usuario);
+            System.out.println("contraseña: "+ contraseña);
+            
+           
+            
+            vect[0]= motor;
+            vect[1]= basededatos;
+            vect[2]= servidor;
+            vect[3]= usuario;
+            vect[4]= contraseña;
+            return vect;
+
+        // Para atrapar excepciones
+        }catch(StringIndexOutOfBoundsException e){
+            
+          
+          
+            System.out.println("No se pudo convertir la ruta a String");
+            System.out.println(e);
+            
+          
+            FrmConfiguracion frmconfig = new FrmConfiguracion();
+            frmconfig.setVisible(true);
+            frmconfig.setAlwaysOnTop(true);
+            
+            
+            
+        } catch (IOException e) {
+            System.out.println("Error al crear el archivo wini");
+            System.out.println("verifique que tenga instalado la libreria correspondiente");
+            
+            
+        }catch(Exception e){
+            
+            System.out.println("error al leer el archivo config.ini ubicado en los recursos ");
+            
+            FrmConfiguracion frmconfig = new FrmConfiguracion();
+            frmconfig.setVisible(true);
+            frmconfig.setAlwaysOnTop(true);
+            
+        }            
+        
+        
+        return null;
+            
+            
+    }
+    
+    
+    public String[] leer(String ruta) {
+        String vect[]=new String[5];
+    
+        try{
+            
+            
+             System.out.println("ruta al leer:"+ruta);
+            
+            
+            Wini ini = new Wini(new File(ruta));
+            String servidor = ini.get("database", "servidor", String.class);
+            String usuario = ini.get("database", "usuario", String.class);
+            String contraseña = ini.get("database", "contraseña", String.class);
+            String motor = ini.get("database", "motor", String.class);
+            String basededatos = ini.get("database", "basededatos", String.class);
+        
+            
+            System.out.println("motor: "+ motor);
+            System.out.println("basededatos: "+ basededatos);
+            System.out.println("servidor: "+ servidor);
+            System.out.println("usuario: "+ usuario);
+            System.out.println("contraseña: "+ contraseña);
+            
+           
+            
+            vect[0]= motor;
+            vect[1]= basededatos;
+            vect[2]= servidor;
+            vect[3]= usuario;
+            vect[4]= contraseña;
+            return vect;
+
+
+            
+            
+            
+                // Para atrapar excepciones
+        }catch (IOException e) {
+            System.out.println("Error al crear el archivo wini");
+            System.out.println("verifique que tenga instalado la libreria correspondiente");
+            FrmConfiguracion frmconfig = new FrmConfiguracion();
+            frmconfig.setVisible(true);
+            frmconfig.setAlwaysOnTop(true);
+            
+            
+        }catch(Exception e){
+            
+            System.out.println("error al leer el archivo config.ini ubicado en los recursos ");
+            
+            FrmConfiguracion frmconfig = new FrmConfiguracion();
+            frmconfig.setVisible(true);
+            frmconfig.setAlwaysOnTop(true);
+            
+        }     
+            
+            
+            
+            
+            
+            
+            
+        return null;
+    
+    }   
+    
+}
