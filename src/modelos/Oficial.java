707a66921581a058b3ae1c45adb4e22878782c6a
Danny_PC <Danny_PC@Equipo-0c10>
2020-03-31 14:15:36 -0600

    clases agregadas

--- src/modelos/Oficial.java
+++ src/modelos/Oficial.java
@@ -5,10 +5,70 @@
  */
 package modelos;
 
+import java.sql.Date;
+
 /**
  *
  * @author Danny_PC
  */
 public class Oficial {
+    private int cedula;
+    private String nombre;
+    private Date fechanacimiento;
+    private int telefono;
+    private String correo;
+    private double salario;
     
+    
+    
+    
+    
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFechanacimiento() {
+        return fechanacimiento;
+    }
+
+    public void setFechanacimiento(Date fechanacimiento) {
+        this.fechanacimiento = fechanacimiento;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
 }
