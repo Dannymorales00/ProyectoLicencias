892082937f84f2433e7e3cdbc3e438a23ecdba83
Danny_PC <Danny_PC@Equipo-0c10>
2020-03-31 14:24:20 -0600

    clases agregadas

---
+++ src/modelos/Cliente.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+import java.sql.Date;
+import sun.security.util.Password;
+
+/**
+ *
+ * @author Danny_PC
+ */
+public class Cliente {
+    private int cedula;
+    private String nombre;
+    private Date fechanacimiento;
+    private int telefono;
+    private String correo;
+    private String sobrenombre;
+    private Password contraseña;
+    private String tipo;//tipo de usuario /secretaria-oficial
+
+    public Cliente(int cedula, String nombre, Date fechanacimiento, int telefono, String correo, String sobrenombre, Password contraseña, String tipo) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.fechanacimiento = fechanacimiento;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.sobrenombre = sobrenombre;
+        this.contraseña = contraseña;
+        this.tipo = tipo;
+    }
+
+    public Cliente() {
+        this.cedula = 0;
+        this.nombre = null;
+        this.fechanacimiento = null;
+        this.telefono = 0;
+        this.correo = null;
+        this.sobrenombre = null;
+        this.contraseña = null;
+        this.tipo = null;
+        
+        
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFechanacimiento() {
+        return fechanacimiento;
+    }
+
+    public void setFechanacimiento(Date fechanacimiento) {
+        this.fechanacimiento = fechanacimiento;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getSobrenombre() {
+        return sobrenombre;
+    }
+
+    public void setSobrenombre(String sobrenombre) {
+        this.sobrenombre = sobrenombre;
+    }
+
+    public Password getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(Password contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+    
+    
+    
+    
+}
