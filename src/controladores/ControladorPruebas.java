f156555d9c6cb4c2c6434f0e297d894a3bdd4f85
LeanPC <LeanPC@DESKTOP-MCHJ7I4>
2020-04-01 13:52:48 -0600

    validaciones hora/fecha agregadas

--- src/controladores/ControladorPruebas.java
+++ src/controladores/ControladorPruebas.java
@@ -107,7 +107,7 @@ public class ControladorPruebas {
     }
     
     //valida que exista un oficial para poder agregar una prueba
-    public boolean ValidarFK(Prueba prueba){
+    public boolean ValidarFKOficial(Prueba prueba){
 
         try 
         {
@@ -121,12 +121,57 @@ public class ControladorPruebas {
             }
                 
         } catch (SQLException ex){
-            System.out.println("Error al validarFK");
+            System.out.println("Error al validarFKOficial");
             
         }
          
         return false;
        
     }  
+    //valida que exista una cita con esa fecha para poder agregar una prueba
+    public boolean ValidarFKFecha(Prueba prueba){
+
+        try 
+        {
+            this.datos = this.sentencias.executeQuery("select * from citas where fecha="+prueba.getFecha());
+                
+            if (datos.next()) 
+            {
+            
+                return true;
+              
+            }
+                
+        } catch (SQLException ex){
+            System.out.println("Error al validarFKFecha");
+            
+        }
+         
+        return false;
        
+    }
+    //valida que exista una cita con esa hora para poder agregar una prueba
+    public boolean ValidarFKHora(Prueba prueba){
+
+        try 
+        {
+            this.datos = this.sentencias.executeQuery("select * from citas where hora="+prueba.getHora());
+                
+            if (datos.next()) 
+            {
+            
+                return true;
+              
+            }
+                
+        } catch (SQLException ex){
+            System.out.println("Error al validarFKHora");
+            
+        }
+         
+        return false;
+       
+    }  
+    
+    
 }
